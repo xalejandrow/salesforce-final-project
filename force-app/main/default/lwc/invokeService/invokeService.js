5d7515fb393bd3a6d9272af2f2d084e36a0bda0c
Alew <xalejandrow@gmail.com>
2022-04-17 18:12:46 -0300

    Add Toastr Nitification

--- force-app/main/default/lwc/invokeService/invokeService.js
+++ force-app/main/default/lwc/invokeService/invokeService.js
@@ -1,4 +1,5 @@
 import { LightningElement , wire} from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ucl_herokuApi from '@salesforce/apex/ucl_HerokuApiHandler.getHerokuData';
 import ucl_herokuDelete from '@salesforce/apex/ucl_HerokuApiHandler.deleteAll';
 
@@ -6,6 +7,15 @@ import ucl_herokuDelete from '@salesforce/apex/ucl_HerokuApiHandler.deleteAll';
 export default class InvokeService extends LightningElement {
     // @wire(ucl_herokuApi)
     clickedButtonLabel = '';
+    _title = 'Import Data';
+    message = '';
+    variant = 'success';
+    variantOptions = [
+        { label: 'error', value: 'error' },
+        { label: 'warning', value: 'warning' },
+        { label: 'success', value: 'success' },
+        { label: 'info', value: 'info' },
+    ];
 
     handleClick(event) {
         try {
@@ -14,9 +24,15 @@ export default class InvokeService extends LightningElement {
         // this.clickedButtonLabel = event.target.label;
         
             this.clickedButtonLabel = "Process Completed";
+            this.message = "Process Completed";
+            this.variant = 'success';
+            this.showNotification();
     
         } catch (error) {
             this.clickedButtonLabel = "Process Error";
+            this.message = "Process Error";
+            this.variant = "error";
+            this.showNotification();
 
         }
         
@@ -30,11 +46,26 @@ export default class InvokeService extends LightningElement {
         // this.clickedButtonLabel = event.target.label;
         
             this.clickedButtonLabel = "Records Deleted";
+            this.message = "Records Deleted";
+            this.variant = "info";
+            this.showNotification();
     
         } catch (error) {
             this.clickedButtonLabel = "Process Error";
+            this.message = "Process Error";
+            this.variant = "error";
+            this.showNotification();
 
         }
         
+    }
+
+    showNotification() {
+        const evt = new ShowToastEvent({
+            title: this._title,
+            message: this.message,
+            variant: this.variant,
+        });
+        this.dispatchEvent(evt);
     }
 }
